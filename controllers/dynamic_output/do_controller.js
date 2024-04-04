17ad7d425d26f5e6351102d4d13962849923ca1e
Vivek Kandoliya <vivekkandoliya123@gmail.com>
2024-04-04 08:50:44 +0530

    controller in dynamic output added

---
+++ controllers/dynamic_output/do_controller.js
@@ -0,0 +1,117 @@
+/*
+Program: dynamic output controller
+Description: main controller of dynamic output task
+Called-by: dynamic_output.js
+@uther: Vivek Kandoliya
+*/
+
+// ====  Importing Node Modules  ====
+const { LocalStorage } = require("node-localstorage");
+const { CLIENT_RENEG_LIMIT } = require('tls');
+
+//=====  Importing Local Modules  =====
+const con = require('../../db');
+
+//===== Setting valriables  ======
+let localstorage = new LocalStorage('./abc');
+
+//Function return fresh_home : returns fresh home page of this task (first time)
+const fresh_home = (req, res) => {
+    try {
+        res.render('dynamic_out/pages/home', { result: false });
+    }
+    catch (error) {
+        console.log('touched : controllers/dynamic_output/do_controller -> fresh_home')
+        console.log(err);
+        return res.render('error_page');
+    }
+}
+
+//Function return post_query : returns output data of the fired query (page 1)
+const post_query = (req, res) => {
+
+    //set set variables using request data
+    let sqlquery = req.body.sqlquery;
+    sqlquery = sqlquery.replace(";", "");
+
+    //storing query into local storage.
+    localstorage.clear();
+    localstorage.setItem("sqlquery", sqlquery);
+
+    let finalsqlquery = sqlquery;
+
+    //storing limit into local storage.
+    let limited = -1;
+    localstorage.setItem('limited', limited);
+    if (!sqlquery.includes('limit') && sqlquery.length != 0) {
+        finalsqlquery = `${sqlquery} limit 0,20`;
+    }
+    else if (sqlquery.length != 0) {
+        destructured = finalsqlquery.split('limit');
+        limited = parseInt(destructured[1]);
+        localstorage.setItem('limited', limited);
+        if (limited < 20) {
+            finalsqlquery = `${destructured[0]} limit 0,${limited}`;
+        }
+        else {
+            finalsqlquery = `${destructured[0]} limit 0,20`;
+        }
+
+    }
+
+    con.query(`${finalsqlquery}; ${sqlquery}`, function (err, result) {
+        console.log(result)
+        if (err) {
+            console.log(err);
+
+            res.render('dynamic_out/pages/home', { result: true, err: true, data: err, query: finalsqlquery });
+        } else {
+
+            res.render('dynamic_out/pages/home', { result: true, err: false, query: finalsqlquery, data: result[0], columns: Object.keys(result[0][0]), page: 1, no_of_records: 20, total_data: result[1].length });
+        }
+    })
+
+}
+
+
+//Function return get_query : returns output data of next pages (other pages)
+const get_query = (req, res) => {
+    let page = req.query.page;
+    if (page == undefined || isNaN(page)) page = 1;
+    let sqlquery = localstorage.getItem('sqlquery');
+    let start = (page * 20) - 20;
+    let limited = localstorage.getItem('limited');
+    let finalsqlquery = sqlquery
+
+    if (!sqlquery.includes('limit')) {
+        finalsqlquery = `${sqlquery} limit ${start},20`;
+    }
+    else if (sqlquery.length != 0) {
+        destructured = finalsqlquery.split('limit');
+        limited = parseInt(destructured[1]);
+        console.log(limited);
+        console.log(start);
+        console.log(start + 20)
+        if (limited < start + 20 && limited > 0) {
+            finalsqlquery = `${destructured[0]} limit ${start},${limited - start}`;
+        }
+        else {
+            finalsqlquery = `${destructured[0]} limit ${start},20`;
+        }
+    }
+
+    console.log(finalsqlquery);
+
+    con.query(`${finalsqlquery}; ${sqlquery}`, function (err, result) {
+        if (err) {
+            res.render('dynamic_out/pages/home', { result: true, err: true, data: err, query: finalsqlquery });
+        } else {
+
+            res.render('dynamic_out/pages/home', { result: true, err: false, query: finalsqlquery, data: result[0], columns: Object.keys(result[0][0]), page: page, no_of_records: 20, total_data: result[1].length });
+        }
+    })
+
+
+}
+
+module.exports = { fresh_home, post_query, get_query }
--- routes/dynamic_output.js
+++ routes/dynamic_output.js
@@ -1,117 +1,33 @@
-
+/*
+Program: Dynamic Output router
+Description: controls all requests belongs to Dynamic Output task
+Called-by: server.js
+@uther: Vivek Kandoliya
+*/
 
 
 //--------------- Importing Node Modules
 const express = require('express');
+const { LocalStorage } = require("node-localstorage");
+const { CLIENT_RENEG_LIMIT } = require('tls');
 
 //------------------Configs
 const router = express.Router();
-const path = require('path');
 const varifyUser = require('../middlewares/varifyUser');
-const con = require('../db');
+const controller = require('../controllers/dynamic_output/do_controller');
+let localstorage = new LocalStorage('./abc');
 
 
 
 
 ////////////////////////////////////////////////////
 
-const { LocalStorage } = require("node-localstorage");
-const { CLIENT_RENEG_LIMIT } = require('tls');
 
-let localstorage = new LocalStorage('./abc');
+router.get('/',varifyUser, controller.fresh_home);
 
-router.get('/',varifyUser, (req, res) => {
-    res.render('dynamic_out/pages/home', { result: false });
-})
+router.post('/query', varifyUser, controller.post_query);
 
-router.post('/query', varifyUser, (req, res) => {  
-
-    let sqlquery = req.body.sqlquery;
-    sqlquery = sqlquery.replace(";", "");
-    localstorage.clear();
-    localstorage.setItem("sqlquery", sqlquery);
-    let finalsqlquery = sqlquery;
-    let limited = -1;
-    localstorage.setItem('limited', limited);
-    if (!sqlquery.includes('limit') && sqlquery.length != 0) {
-        finalsqlquery = `${sqlquery} limit 0,20`;
-    }
-    else if(sqlquery.length != 0){
-        destructured = finalsqlquery.split('limit');
-        limited = parseInt(destructured[1]);
-        localstorage.setItem('limited', limited);
-        if(limited<20){
-            finalsqlquery = `${destructured[0]} limit 0,${limited}`;
-        }
-        else{
-            finalsqlquery = `${destructured[0]} limit 0,20`;
-        }
-        
-    }
-
-    
-
-    con.query(`${finalsqlquery}; ${sqlquery}`, function (err, result) {
-        console.log(result)
-        if (err) {
-            console.log(err);
-            
-            res.render('dynamic_out/pages/home', { result: true, err: true, data: err , query: finalsqlquery });
-        } else {
-           
-            res.render('dynamic_out/pages/home', { result: true, err: false,query: finalsqlquery, data: result[0], columns: Object.keys(result[0][0]), page: 1, no_of_records: 20, total_data: result[1].length });
-        }
-    })
-    
-
-    // con.query(finalsqlquery, function (err, result) {
-    //     if (err) {
-    //         res.render('dynamic_out/pages/home', { result: true, err: true,query: finalsqlquery, data: err });
-    //     } else {
-    //         console.log(result[0]);
-    //         res.render('dynamic_out/pages/home', { result: true, err: false,query: finalsqlquery, data: result, columns: Object.keys(result[0]), page: 1, no_of_records: 20, total_data: 50000});
-    //     }
-    // })
-})
-
-router.get('/query', varifyUser, (req, res) => {
-    let page = req.query.page;
-    if (page == undefined || isNaN(page)) page = 1;
-    let sqlquery = localstorage.getItem('sqlquery');
-    let start = (page * 20) - 20;
-    let limited = localstorage.getItem('limited');
-    let finalsqlquery = sqlquery
-
-    if(!sqlquery.includes('limit')){
-        finalsqlquery = `${sqlquery} limit ${start},20`;
-    }
-    else if(sqlquery.length != 0){
-        destructured = finalsqlquery.split('limit');
-        limited = parseInt(destructured[1]);
-        console.log(limited);
-        console.log(start);
-        console.log(start + 20) 
-        if(limited < start + 20 && limited>0){
-            finalsqlquery = `${destructured[0]} limit ${start},${limited-start}`;
-        }
-        else{
-            finalsqlquery = `${destructured[0]} limit ${start},20`;
-        }
-    }
-   
-    console.log(finalsqlquery);
-    
-    con.query(`${finalsqlquery}; ${sqlquery}`, function (err, result) {
-        if (err) {
-            res.render('dynamic_out/pages/home', { result: true, err: true, data: err , query: finalsqlquery });
-        } else {
-           
-            res.render('dynamic_out/pages/home', { result: true, err: false,query: finalsqlquery, data: result[0], columns: Object.keys(result[0][0]), page: page, no_of_records: 20, total_data: result[1].length });
-        }
-    })
-
-
-})
+router.get('/query', varifyUser, controller.get_query)
 
 ////////////////////////////////////////////////////
 module.exports = router;
