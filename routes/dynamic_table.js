e3c01306e7f4b58c7551602d3019a034303503d7
vivek-workspace <vivek.kandoliya.2024@gmail.com>
2024-04-04 11:45:08 +0530

    controller for task : dynamic table added.

--- routes/dynamic_table.js
+++ routes/dynamic_table.js
@@ -1,15 +1,33 @@
+/*
+Program: fetching dynamic_table router
+Description: controls all requests belongs to fdynamic_table task
+Called-by: server.js
+@uther: Vivek Kandoliya
+*/
 
-//--------------- Importing Node Modules
+// =====  Importing Node Modules =====
 const express = require('express');
-
-//------------------Configs
-const router = express.Router();
 const path = require('path');
+
+// =====  Importing Local Modules  =====
 const varifyUser = require('../middlewares/varifyUser');
 
-//----------------End Points (Login Required)
+// ===== Setting Variables  =====
+const router = express.Router();
+
+// ========== Endpoint Section (Login Required in All) (/tasks/attendence_result/) ==========
+
+// End-point 1 : /tasks/dynamic_table/ (login required)
+//Desc: returns html file of the dynamic table task
 router.get('/', varifyUser, (req,res) => { 
-    res.sendFile(path.join(__dirname, '../views/Dynamic_Table_JS_T2.html'));
+    try{
+        res.sendFile(path.join(__dirname, '../views/Dynamic_Table_JS_T2.html'));
+    }
+    catch(error){
+        console.log('touched : Routes/dynamic_table -> router.get("/")')
+        console.log(error);
+        res.render('error_page');
+    }
 })
 
-module.exports = router;
+module.exports = router
