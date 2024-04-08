afa704c4948db9d588e75474c1e1b480f8648637
Vivek Kandoliya <vivekkandoliya123@gmail.com>
2024-04-08 08:40:34 +0530

    added some css

--- Server.js
+++ Server.js
@@ -37,6 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/views')));
+app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
 
 app.set('view engine','ejs');
 
